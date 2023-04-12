72ab5c1e9b40b7174763cd66c84dbfb89c918bc9
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2023-04-12 19:09:53 +0300

    little changes

--- lab4_ov-r/l4.js
+++ lab4_ov-r/l4.js
@@ -1,27 +1,3 @@
-function showWeeks() {
-    // створюємо об'єкт дати для поточної дати
-    let currentDate = new Date();
-    // створюємо об'єкт дати для 1 вересня поточного року
-    let firstSeptember = new Date(currentDate.getFullYear(), 8, 1);
-    // якщо поточна дата перед 1 вересням, ми повинні використовувати минулий рік
-    if (currentDate < firstSeptember) {
-        firstSeptember.setFullYear(currentDate.getFullYear() - 1);
-    }
-    // визначаємо різницю в мілісекундах між 1 вересням і поточною датою
-    let millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-    console.log(millisecondsPerWeek);
-    let difference = currentDate.getTime() - firstSeptember.getTime();
-    console.log(difference);
-    // визначаємо кількість тижнів з останнього 1 вересня
-    let weeks = Math.floor(difference / millisecondsPerWeek);
-    console.log(weeks);
-    // встановлюємо значення input з id "weeks"
-    document.getElementById("weeks").value = weeks;
-}
-function clearWeeks() {
-    document.getElementById("weeks").value = "";
-}
-
 function leapYear() {
     let year = new Date().getFullYear();
     console.log(year);
